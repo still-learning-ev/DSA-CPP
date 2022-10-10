1528e38a56087ae31b5d5d77c73aecb38cbf4f11
zeeshan lone <56621467+still-learning-ev@users.noreply.github.com>
2022-10-10 17:54:31 +0530

    Add files via upload
    program to find whether the input character is uppercase lowercase or numeric

---
+++ lec3uppercase.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   //program to check if the input is numeric uppercase or lower
+//   cout<<"enter the character or number "<<endl;
+//   char ch;
+//   cin>>ch;
+//   if(ch>='a' && ch<='z'){
+//     cout<<"lower case letter"<<endl;
+//   }else if(ch>='A' && ch<='Z'){
+//     cout<<"upper case letter"<<endl;
+//   }else if(ch>=0 && ch<='9'){
+//     cout<<"numeric character"<<endl;
+//   }
+//   return 0;
+// }
